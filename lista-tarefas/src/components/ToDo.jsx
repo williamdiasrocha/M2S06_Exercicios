d48e31d727b9d132af20663594554fb3e5ec3c24
William Rocha <uill_pf@hotmail.com>
2023-06-18 10:02:23 -0300

    Ex3 - Corrigido

--- lista-tarefas/src/components/ToDo.jsx
+++ lista-tarefas/src/components/ToDo.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const ToDo = () => {
-    return <div></div>;
+  return (
+    <div>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
 };
 
 export default ToDo;
